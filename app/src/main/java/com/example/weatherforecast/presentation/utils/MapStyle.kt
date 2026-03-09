db026b3c20d9bb23b614e4c951b38183cbe4b05c
Hendsayed25 <hendsayed853@gmail.com>
2026-03-09 22:47:42 +0200

    Add MapScreen and MapViewModel for location selection

---
+++ app/src/main/java/com/example/weatherforecast/presentation/utils/MapStyle.kt
@@ -0,0 +1,21 @@
+package com.example.weatherforecast.presentation.utils
+
+object MapStyle {
+
+    object Enliberte {
+        const val BRIGHT = "https://tuiles.enliberte.fr/styles/bright.json"
+        const val BASIC = "https://tuiles.enliberte.fr/styles/basic.json"
+        const val MAP_TILER_3d = "https://tuiles.enliberte.fr/styles/maptiler-3d.json"
+        const val POSITRON = "https://tuiles.enliberte.fr/styles/positron.json"
+        const val LIBERTY = "https://tuiles.enliberte.fr/styles/liberty.json"
+        const val FIORD = "https://tuiles.enliberte.fr/styles/fiord.json"
+        const val DARK = "https://tuiles.enliberte.fr/styles/dark.json"
+        const val TONER = "https://tuiles.enliberte.fr/styles/toner.json"
+    }
+
+    object OpenFreeMap {
+        const val POSITRON = "https://tiles.openfreemap.org/styles/positron"
+        const val BRIGHT = "https://tiles.openfreemap.org/styles/bright"
+        const val LIBERTY = "https://tiles.openfreemap.org/styles/liberty"
+    }
+}
