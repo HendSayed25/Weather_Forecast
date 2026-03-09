2cad837e9500cf35fd519ac53bf9db7cd5a57a47
Hendsayed25 <hendsayed853@gmail.com>
2026-03-09 22:46:26 +0200

    Add getLocationDetails to LocationRepository and favorites support to WeatherRepository

--- app/src/main/java/com/example/weatherforecast/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherforecast/data/repository/WeatherRepository.kt
@@ -1,13 +1,17 @@
 package com.example.weatherforecast.data.repository
 
+import com.example.weatherforecast.data.local.datasource.WeatherLocalDataSource
+import com.example.weatherforecast.data.local.entity.Weather
 import com.example.weatherforecast.data.remote.datasource.WeatherRemoteDataSource
 import com.example.weatherforecast.data.remote.response.ForecastResponse
 import com.example.weatherforecast.data.remote.response.WeatherResponse
 import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
 
 
 class WeatherRepository @Inject constructor(
-    private val weatherRemoteDataSource: WeatherRemoteDataSource
+    private val weatherRemoteDataSource: WeatherRemoteDataSource,
+    private val weatherLocalDataSource: WeatherLocalDataSource
 ) {
     suspend fun getCurrentWeather(lat: Double, long: Double): Result<WeatherResponse> {
         return weatherRemoteDataSource.getCurrentWeather(lat, long)
@@ -15,5 +19,17 @@ class WeatherRepository @Inject constructor(
 
     suspend fun getForecastWeather(lat: Double, long: Double): Result<ForecastResponse> {
         return weatherRemoteDataSource.getForecastWeather(lat, long)
+    }
+
+    suspend fun addWeatherToFav(lat: Double, long: Double,cityName : String , countryName : String){
+        weatherLocalDataSource.addWeather(Weather(lat = lat, long = long, cityName = cityName, cityDescription = countryName))
+    }
+
+    suspend fun deleteWeatherFromFav(weather: Weather){
+        weatherLocalDataSource.deleteWeather(weather)
+    }
+
+    fun getAllFavWeathers() : Flow<List<Weather>>{
+        return weatherLocalDataSource.getAllWeathers()
     }
 }
