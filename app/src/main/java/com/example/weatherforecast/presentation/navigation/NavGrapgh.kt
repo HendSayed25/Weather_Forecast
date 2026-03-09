945bfe55c22dc70625330af08d1e109a33524337
Hendsayed25 <hendsayed853@gmail.com>
2026-03-09 22:46:38 +0200

    Add MapScreen route and provide NavController via CompositionLocal

--- app/src/main/java/com/example/weatherforecast/presentation/navigation/NavGrapgh.kt
+++ app/src/main/java/com/example/weatherforecast/presentation/navigation/NavGrapgh.kt
@@ -1,10 +1,15 @@
 package com.example.weatherforecast.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.weatherforecast.presentation.favorite.FavoriteScreen
+import com.example.weatherforecast.presentation.map.MapScreen
 import com.example.weatherforecast.presentation.home.HomeScreen
 
 @Composable
@@ -14,13 +19,23 @@ fun NavGraph(
 ) {
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = Route.HomeRoute,
-        modifier = modifier
-    ) {
-        composable<Route.HomeRoute> {
-            HomeScreen(modifier = modifier, locationGranted = locationGranted)
+    CompositionLocalProvider(LocalNavController provides navController) {
+        NavHost(
+            navController = navController,
+            startDestination = Route.HomeRoute,
+            modifier = modifier
+        ) {
+            composable<Route.HomeRoute> {
+                HomeScreen(locationGranted = locationGranted)
+            }
+
+            composable<Route.MapScreen> {
+                MapScreen()
+            }
         }
     }
+}
+
+val LocalNavController = compositionLocalOf<NavController> {
+    error("NavController not provided")
 }
