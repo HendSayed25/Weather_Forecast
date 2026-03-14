9e4d358aaf3d4ac807acabdadb0fa67f2f2cb614
Hendsayed25 <hendsayed853@gmail.com>
2026-03-14 23:46:06 +0200

    Configure WorkManager with Hilt and provide WorkManager in AppModule

--- app/src/main/java/com/example/weatherforecast/application.kt
+++ app/src/main/java/com/example/weatherforecast/application.kt
@@ -1,7 +1,18 @@
 package com.example.weatherforecast
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class WeatherApp : Application()
+class WeatherApp : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
