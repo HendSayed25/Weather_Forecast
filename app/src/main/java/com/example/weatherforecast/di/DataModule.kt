6b27c912a59defcf39410a33419e5b9c3bd47ff7
Hendsayed25 <hendsayed853@gmail.com>
2026-03-16 04:50:31 +0200

    Consolidate database, DataStore, and network dependency injection into a single DataModule

--- app/src/main/java/com/example/weatherforecast/di/DataModule.kt
+++ app/src/main/java/com/example/weatherforecast/di/DataModule.kt
@@ -1,20 +1,49 @@
-package com.example.weatherforecast.data.di
+package com.example.weatherforecast.di
 
 import android.content.Context
 import androidx.room.Room
 import com.example.weatherforecast.data.local.dao.AlertDao
 import com.example.weatherforecast.data.local.dao.WeatherDao
+import com.example.weatherforecast.data.local.datastore.AppDataStore
 import com.example.weatherforecast.data.local.db.AppDatabase
+import com.example.weatherforecast.data.remote.network.WeatherApiService
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseModule {
+object DataModule {
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+    @Provides
+    @Singleton
+    fun provideAppDataStore(@ApplicationContext context: Context): AppDataStore {
+        return AppDataStore(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(
+                GsonConverterFactory.create(GsonBuilder().serializeNulls().create())
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideService(retrofit: Retrofit): WeatherApiService {
+        return retrofit.create(WeatherApiService::class.java)
+    }
 
     @Provides
     @Singleton
