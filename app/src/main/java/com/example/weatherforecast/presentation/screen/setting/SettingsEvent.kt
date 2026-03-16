a3af2e49298fce6d06a6c31daa39333356dfd666
Hendsayed25 <hendsayed853@gmail.com>
2026-03-16 16:43:22 +0200

    Remove RevokeLocationPermission event and add settings string resources

--- app/src/main/java/com/example/weatherforecast/presentation/screen/setting/SettingsEvent.kt
+++ app/src/main/java/com/example/weatherforecast/presentation/screen/setting/SettingsEvent.kt
@@ -3,6 +3,5 @@ package com.example.weatherforecast.presentation.screen.setting
 sealed class SettingsEvent{
     data object NavigateToMapScreen : SettingsEvent()
     object RequestLocationPermission : SettingsEvent()
-    data object RevokeLocationPermission : SettingsEvent()
     data class ShowSnackbar(val message: String) : SettingsEvent()
 }
