67e0daeb7d4bbf557b74ccf1cfc0a42829fa9094
Hendsayed25 <hendsayed853@gmail.com>
2026-03-16 19:13:10 +0200

    Update NavGraph to support RTL layout direction based on language

--- app/src/main/java/com/example/weatherforecast/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/weatherforecast/presentation/navigation/NavGraph.kt
@@ -8,10 +8,13 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.weatherforecast.data.local.datastore.Language
 import com.example.weatherforecast.presentation.screen.alert.AlertScreen
 import com.example.weatherforecast.presentation.screen.favorite.FavoriteScreen
 import com.example.weatherforecast.presentation.screen.home.HomeScreen
@@ -23,8 +26,10 @@ import com.example.weatherforecast.worker.Constants.ALERT_NAVIGATE_VALUE
 fun NavGraph(
     modifier: Modifier = Modifier,
     navigateTo: String? = null,
+    language: Language = Language.ENGLISH,
 ) {
     val navController = rememberNavController()
+    val layoutDirection = if (language == Language.ARABIC) LayoutDirection.Rtl else LayoutDirection.Ltr
 
     LaunchedEffect(navigateTo) {
         if (navigateTo == ALERT_NAVIGATE_VALUE) {
@@ -32,10 +37,11 @@ fun NavGraph(
         }
     }
 
-    CompositionLocalProvider(LocalNavController provides navController) {
+    CompositionLocalProvider(
+        LocalNavController provides navController, LocalLayoutDirection provides layoutDirection
+    ) {
         Scaffold(
-            bottomBar = { BottomNavBar(navController) }
-        ) { paddingValues ->
+            bottomBar = { BottomNavBar(navController) }) { paddingValues ->
             NavHost(
                 navController = navController,
                 startDestination = Route.HomeRoute(),
@@ -57,7 +63,7 @@ fun NavGraph(
                     AlertScreen()
                 }
 
-                composable <Route.SettingRoute>{
+                composable<Route.SettingRoute> {
                     SettingScreen()
                 }
             }
