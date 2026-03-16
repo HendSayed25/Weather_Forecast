36ad01cb717aecb682ff1c891ca6a1b83f89c5df
Hendsayed25 <hendsayed853@gmail.com>
2026-03-16 19:13:18 +0200

    Update MainActivity to support dynamic language switching using AppDataStore

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -4,28 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.weatherforecast.data.local.datastore.AppDataStore
+import com.example.weatherforecast.data.local.datastore.Language
 import com.example.weatherforecast.designsystem.theme.WeatherForecastTheme
 import com.example.weatherforecast.presentation.navigation.NavGraph
+import com.example.weatherforecast.presentation.utils.LanguageUtilUtils.setAppLocale
 import com.example.weatherforecast.worker.Constants.ALERT_NAVIGATE_KEY
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val navigateTo = mutableStateOf<String?>(null)
+    @Inject
+    lateinit var appDataStore: AppDataStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         navigateTo.value = intent.getStringExtra(ALERT_NAVIGATE_KEY)
-
         setContent {
+            val language by appDataStore.language.collectAsStateWithLifecycle(Language.ENGLISH)
+            this.setAppLocale(language.code)
+
             WeatherForecastTheme {
                 NavGraph(
                     modifier = Modifier,
-                    navigateTo = navigateTo.value
+                    navigateTo = navigateTo.value,
+                    language = language
                 )
             }
         }
